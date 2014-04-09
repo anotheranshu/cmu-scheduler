6e56c6b52a3f59e55cb4ab04d1ec2859346e3e31
anotheranshu <anotheranshu@gmail.com>
2014-04-09 15:34:27 -0400

    Let's try to add some courses, again

--- schedule/helpers.py
+++ schedule/helpers.py
@@ -295,7 +295,8 @@ def import_courses():
 	raw_json = (open(settings.PROJECT_PATH + "/schedule/static/data/courses.json")).read()
 	data = json.loads(raw_json)
 	for course in data:
-		mydescription = course.get("description", "")  
+		mydescription = course.get("description", "")
+		mytitle = course.get("title", "")  
 		dcourse = Course(course_id=course["courseId"], tree_type=course["treeType"], is_starter=course["isStarter"], prereq_indices=i_list_to_CSL(course["prereqIndices"]), postreq_indices=i_list_to_CSL(course["postreqIndices"]), description=mydescription, node_id=course["nodeId"], title=course["title"])
 		dcourse.save()
 
