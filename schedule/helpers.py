91c8ccf95267deb544ef8bf31360c3f4cb390491
anotheranshu <anotheranshu@gmail.com>
2014-04-16 20:31:20 -0400

    tabs and spaces and shit messing things up with Python.

--- schedule/helpers.py
+++ schedule/helpers.py
@@ -15,48 +15,48 @@ ACTIVITY_VALUE = 1
 GROUP_SIZE = 4
 
 def i_list_to_CSL(l):
-	if len(l) == 0:
-		return ""
-	return (",".join(map(lambda n: str(n), l)))
+  if len(l) == 0:
+    return ""
+  return (",".join(map(lambda n: str(n), l)))
 
 def diff(a, b):
-	b = set(b)
-	return [aa for aa in a if aa not in b]
+  b = set(b)
+  return [aa for aa in a if aa not in b]
 
 def coursenum_to_id(n):
-	return Course.objects.get(course_num=n).node_id
+  return Course.objects.get(course_num=n).node_id
 
 #Creates a new student with the given parameters and saves them to the database (with a User for them)
 def make_student(myandrew, mypassword):
-	u = User.objects.create_user(myandrew, (myandrew + "@andrew.cmu.edu"), myandrew, first_name=myandrew, last_name=myandrew)
-	student_data = audit.get_all_courses(myandrew, mypassword)
+  u = User.objects.create_user(myandrew, (myandrew + "@andrew.cmu.edu"), myandrew, first_name=myandrew, last_name=myandrew)
+  student_data = audit.get_all_courses(myandrew, mypassword)
   mycourses = map(lambda n: coursenum_to_id(n), student_data[0])
   mysems_left = 6
   if (student_data[1] == "Sophomore"):
-  	sems_left = 4
+    sems_left = 4
   elif (student_data[1] == "Junior"):
-  	sems_left = 2
+    sems_left = 2
   else:
-  	pass
+    pass
   myis_cs = (not bool(student_data[2]))
-	student = Student(user=u, andrew=myandrew, taken_courses=mycourses, sems_left=mysems_left, is_cs=myis_cs)
-	student.save()
-	return student
+  student = Student(user=u, andrew=myandrew, taken_courses=mycourses, sems_left=mysems_left, is_cs=myis_cs)
+  student.save()
+  return student
 
 def import_courses():
-	raw_json = (open(settings.PROJECT_PATH + "/schedule/static/data/courses.json")).read()
-	data = json.loads(raw_json)
-	for course in data:
-		mydescription = course.get("description", "")
-		mytitle = course.get("title", "")  
-		myid = course.get("courseId", "")
-		mytree = course.get("treeType", "")
-		mystarter = course.get("isStarter", False)
-		myprereq_indices = i_list_to_CSL(course.get("prereqIndices", ""))
-		mypostreq_indices = i_list_to_CSL(course.get("postreqIndices", ""))
-		mynodeid = course["nodeId"] # We *should* fail if there's no node ID.  There is no default value for this.
-		dcourse = Course(course_id=myid, tree_type=mytree, is_starter=mystarter, prereq_indices=myprereq_indices, postreq_indices=mypostreq_indices, description=mydescription, node_id=mynodeid, title=mytitle)
-		dcourse.save()
+  raw_json = (open(settings.PROJECT_PATH + "/schedule/static/data/courses.json")).read()
+  data = json.loads(raw_json)
+  for course in data:
+    mydescription = course.get("description", "")
+    mytitle = course.get("title", "")  
+    myid = course.get("courseId", "")
+    mytree = course.get("treeType", "")
+    mystarter = course.get("isStarter", False)
+    myprereq_indices = i_list_to_CSL(course.get("prereqIndices", ""))
+    mypostreq_indices = i_list_to_CSL(course.get("postreqIndices", ""))
+    mynodeid = course["nodeId"] # We *should* fail if there's no node ID.  There is no default value for this.
+    dcourse = Course(course_id=myid, tree_type=mytree, is_starter=mystarter, prereq_indices=myprereq_indices, postreq_indices=mypostreq_indices, description=mydescription, node_id=mynodeid, title=mytitle)
+    dcourse.save()
 
-	for course in Course.objects.all():
-		print course
+  for course in Course.objects.all():
+    print course
--- schedule/models.py
+++ schedule/models.py
@@ -3,25 +3,25 @@ from django.contrib.auth.models import User
 
 # Some helper functions.  Yeah, this is probably bad style.
 def problem_field_to_list(myfield):
-	return map(lambda x: int(x), myfield.split())
+  return map(lambda x: int(x), myfield.split())
 
 # Create your models here.
 
 class Course(models.Model):
-	course_num = models.IntegerField(default=0)
-	tree_type = models.CharField(max_length=10)
-	is_starter = models.BooleanField(default=False)
-	prereq_indices = models.CommaSeparatedIntegerField(max_length=150)
-	postreq_indices = models.CommaSeparatedIntegerField(max_length=150)
-	description = models.CharField(max_length=9001)
-	node_id = models.IntegerField(primary_key=True)
-	title = models.CharField(max_length=200)
-	def __unicode__(self):
-		return (self.course_id + ": " + self.title)
+  course_num = models.IntegerField(default=0)
+  tree_type = models.CharField(max_length=10)
+  is_starter = models.BooleanField(default=False)
+  prereq_indices = models.CommaSeparatedIntegerField(max_length=150)
+  postreq_indices = models.CommaSeparatedIntegerField(max_length=150)
+  description = models.CharField(max_length=9001)
+  node_id = models.IntegerField(primary_key=True)
+  title = models.CharField(max_length=200)
+  def __unicode__(self):
+    return (self.course_id + ": " + self.title)
 
 class Student(models.Model):
-	user = models.OneToOneField(User)
-	andrew = models.CharField(max_length=100)
-	taken_courses = models.CommaSeparatedIntegerField(max_length=3000)
-	is_cs = models.BooleanField(default=True)
-	sems_left = models.IntegerField(default=8)
+  user = models.OneToOneField(User)
+  andrew = models.CharField(max_length=100)
+  taken_courses = models.CommaSeparatedIntegerField(max_length=3000)
+  is_cs = models.BooleanField(default=True)
+  sems_left = models.IntegerField(default=8)
