d324c6c54b29431082ee9945375a4f0449a39edb
Oguz Ulgen <oulgen7@gmail.com>
2014-04-20 18:41:34 -0400

    Probably finished the script

--- schedule/scripts/schedule_to_json.py
+++ schedule/scripts/schedule_to_json.py
@@ -1,8 +1,9 @@
 import json
 import graph
+import cs_course_giver
 
 # Requires: 2d array of courses
-def getJson(courses):
+def getJsonVals(courses):
     result = []
     for year in xrange(len(courses)):
         result += [{"name" : str(year), \
@@ -12,7 +13,9 @@ def getJson(courses):
     return json.dumps({"name" : "schedule", "children" : result}, \
                       sort_keys=False, indent=4, separators=(',', ': '))
 
-def main():
-    print getJson(map (lambda x : list(x), graph.restSchedule()))
-
-main()
+def getJson(USERNAME, PASSWORD):
+    courses = cs_course_giver.giveCoursesForUser(USERNAME, PASSWORD)
+    thresholdHrs = 35
+    f = open('flare.json', 'w')
+    s = getJsonVals(map (lambda x : list(x), graph.restSchedule(courses, thresholdHrs)))
+    f.write(s)
